7b5ad0846ddd7cf1b0509d85de1a08014d44dc48
Leonardo Gomes <leonardo.gomes@s2it.com.br>
2017-09-19 14:03:53 -0300

    added example configuration file

---
+++ hdmicfg.lua
@@ -0,0 +1,28 @@
+return {
+	default_order = {
+		"multi-s2",
+		"solo"
+	},
+	["solo"] = {
+		minimum = 1,
+		main = "LVDS1",
+		["HDMI1"] = {
+			"--auto",
+			"--off",
+		},
+		["LVDS1"] = {
+			"--auto",
+		}
+	},
+	["multi-s2"] = {
+		minimum = 2,
+		main = "LVDS1",
+		["HDMI1"] = {
+			"--auto",
+			["left-of"] = "LVDS1",
+		},
+		["LVDS1"] = {
+			pos = "1920x500",
+		},
+	}
+}
